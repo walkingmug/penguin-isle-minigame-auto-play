90af2dc6a51b9e05c4a8389541481ded1721dad6
walkingmug <walkingmug@users.noreply.github.com~>
2023-03-24 23:45:27 +0100

    added window error handling and a delay

--- penguin-isle-minigame-auto-play/image_input/get_image_from_software.py
+++ penguin-isle-minigame-auto-play/image_input/get_image_from_software.py
@@ -1,14 +1,35 @@
+"""Takes a screenshot of a specified screensharing software.
+"""
 import win32gui
+import win32con
+import time
 from PIL import ImageGrab
 
-# Find the handle of the Zoom window
-zoom_hwnd = win32gui.FindWindow(None, "Zoom Meeting")
 
-# Get the coordinates of the Zoom window
-(left, top, right, bottom) = win32gui.GetWindowRect(zoom_hwnd)
+def get_image_from_software(window_name="Zoom Meeting"):
+    """Takes a screenshot from a software window and saves it.
 
-# Take a screenshot of the Zoom window
-screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
+    :raises ValueError: if the window is not found
+    """
+    # Find the handle of the window
+    zoom_hwnd = win32gui.FindWindow(None, window_name)
+    if zoom_hwnd == 0:
+        raise ValueError(f"Window \"{window_name}\" does not exist.")
 
-# Save the screenshot to a file
-screenshot.save('temp\screenshots\zoom_screenshot.png')
+    # Display the window so that it can take a screenshot
+    win32gui.ShowWindow(zoom_hwnd, win32con.SW_SHOWNORMAL)
+
+    # Give some time for the window to show
+    time.sleep(1)
+
+    # Get the coordinates of the window
+    (left, top, right, bottom) = win32gui.GetWindowRect(zoom_hwnd)
+
+    # Take a screenshot of the window
+    screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
+
+    # Minimize the window
+    win32gui.ShowWindow(zoom_hwnd, win32con.SW_MINIMIZE)
+
+    # Save the screenshot to a file
+    screenshot.save('temp\screenshots\screenshot.png')

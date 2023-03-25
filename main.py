20e63faf3dd85e3074d76d5f8352bdcfb30a3170
walkingmug <walkingmug@users.noreply.github.com~>
2023-03-25 12:32:46 +0100

    add get markings events and main function

---
+++ main.py
@@ -0,0 +1,10 @@
+from penguin_isle_minigame_auto_play.image_input.get_markings import get_markings
+
+
+def main() -> None:
+    # run the function to get the markings
+    get_markings()
+
+
+if __name__=="__main__":
+    main()
--- penguin_isle_minigame_auto_play/image_input/get_markings.py
+++ penguin_isle_minigame_auto_play/image_input/get_markings.py
@@ -3,14 +3,14 @@ import cv2
 
 def click_event(event, x, y, flags, params):
    if event == cv2.EVENT_LBUTTONDOWN:
-      print(f'({x},{y})')
+      print(f'X: {x}, Y: {y}')
       
       # put coordinates as text on the image
-      font = cv2.FONT_HERSHEY_SIMPLEX
-      cv2.putText(img, str(x) + ',' + str(y), (x,y), font, 1, (255, 0, 0), 2)
+    #   font = cv2.FONT_HERSHEY_SIMPLEX
+    #   cv2.putText(img, str(x) + ',' + str(y), (x,y), font, 1, (255, 0, 0), 2)
       
-      # show the updated image
-      cv2.imshow('image', img)
+    #   # show the updated image
+    #   cv2.imshow('image', img)
 
 
 def get_markings():
@@ -30,6 +30,4 @@ def get_markings():
     # # Calculate the distance between markings
     # distance = ((int(x2) - int(x1))**2 + (int(y2) - int(y1))**2)**0.5
     # print(f"The distance between the marks is {distance} pixels.")
-
-get_markings()
 

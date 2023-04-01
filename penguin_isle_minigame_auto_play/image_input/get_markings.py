880abf76d01de6bdcdb07a639e1f938da83b9911
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-01 23:30:58 +0200

    fix: image exiting immediately

--- penguin_isle_minigame_auto_play/image_input/get_markings.py
+++ penguin_isle_minigame_auto_play/image_input/get_markings.py
@@ -1,10 +1,27 @@
 import cv2
 
 
+click_counter = 0
+click_coords = {'X1': None, 'Y1': None, 'X2': None, 'Y2': None}
+
+
 def click_event(event, x, y, flags, params):
-   if event == cv2.EVENT_LBUTTONDOWN:
-      print(f'X: {x}, Y: {y}')
-      
+    global click_counter
+    global click_coords
+
+    # Get click coordinates and stop after two clicks
+    if event == cv2.EVENT_LBUTTONDOWN:      
+        click_counter += 1
+        if click_counter == 1:
+            click_coords.update({'X1': x, 'Y1': y})
+        else:
+            click_coords.update({'X2': x, 'Y2': y})
+            cv2.destroyAllWindows()
+        
+        print(click_coords)
+    
+
+
       # put coordinates as text on the image
     #   font = cv2.FONT_HERSHEY_SIMPLEX
     #   cv2.putText(img, str(x) + ',' + str(y), (x,y), font, 1, (255, 0, 0), 2)
@@ -21,13 +38,12 @@ def get_markings():
     # Read the markings
     cv2.setMouseCallback('image', click_event)
 
-    # Exit on keypress
+    # wait for a key to be pressed to exit
     cv2.waitKey(0)
-    
-    # lose the window
+  
+    # close the window
     cv2.destroyAllWindows()
 
     # # Calculate the distance between markings
     # distance = ((int(x2) - int(x1))**2 + (int(y2) - int(y1))**2)**0.5
     # print(f"The distance between the marks is {distance} pixels.")
-

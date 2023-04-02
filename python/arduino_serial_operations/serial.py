610da017bbd701be450f1de058e539abe0e1b24e
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-02 13:30:12 +0200

    feat: add set port and baud rate

--- python/arduino_serial_operations/serial.py
+++ python/arduino_serial_operations/serial.py
@@ -7,5 +7,11 @@ class Serial:
         self.ser = serial.Serial(port, baud_rate)
         time.sleep(2)
     
+    def set_baud_rate(self, baud_rate: int):
+        self.baud_rate = baud_rate
+
+    def set_port(self, port: str):
+        self.port = port
+
     def write_to_serial(self, text: str):
         self.ser.write(text.encode('utf-8'))

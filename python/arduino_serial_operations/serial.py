10dd910d70c9f883e2c43d5f9e8754c5cf021686
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-02 13:50:03 +0200

    feat: serial class

--- python/arduino_serial_operations/serial.py
+++ python/arduino_serial_operations/serial.py
@@ -6,12 +6,12 @@ class Serial:
     def __init__(self, port='COM3', baud_rate=9600):
         self.ser = serial.Serial(port, baud_rate)
         time.sleep(2)
-    
-    def set_baud_rate(self, baud_rate: int):
-        self.baud_rate = baud_rate
 
     def set_port(self, port: str):
-        self.port = port
+        self.ser.port = port
+
+    def set_baud_rate(self, baud_rate: int):
+        self.ser.baudrate = baud_rate
 
     def write_to_serial(self, text: str):
         self.ser.write(text.encode('utf-8'))
--- python/arduino_serial_operations/serial_writing.py
+++
@@ -1,7 +0,0 @@
-from python.arduino_serial_operations.serial_init import initialize_serial
-import serial
-import time
-
-
-def write_to_serial(ser):
-    ser.write()

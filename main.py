2ce8219fa7a689fd34845b16b071f819e9f8cf77
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-05 23:52:10 +0200

    feat: initiate auto marker selector

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from python.arduino_serial_operations.serial_operations import ArduinoSerial
 
 
 def main() -> None:
+    pass
     # save a screenshot image from a given software
-    get_image_from_software("Zoom - Google Chrome")
+    # get_image_from_software("Zoom - Google Chrome")
 
     # set the marks by clicking on the image
-    x1, y1, x2, y2 = get_markings()
+    # x1, y1, x2, y2 = get_markings()
 
     # calculate the distance between the two marks
-    distance_between_marks = get_distance_in_pixels(x1, y1, x2, y2)
+    # distance_between_marks = get_distance_in_pixels(x1, y1, x2, y2)
 
     # convert the distance to seconds
-    push_duration = get_push_duration_from_distance(distance_between_marks)
+    #push_duration = get_push_duration_from_distance(distance_between_marks)
 
     # perform movement on the servo
-    servo = ArduinoSerial()
-    servo.move_servo(push_duration)
+    # servo = ArduinoSerial()
+    # servo.move_servo(push_duration)
 
 
 if __name__=="__main__":

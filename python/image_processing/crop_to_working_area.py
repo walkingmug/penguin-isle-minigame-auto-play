c5d4f8c54be6f6f13805857b811d51f474a30873
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-09 13:00:30 +0200

    feat: crop screenshare to middle part

--- python/image_processing/crop_to_working_area.py
+++ python/image_processing/crop_to_working_area.py
@@ -2,7 +2,28 @@ import cv2
 import numpy as np
 
 
-def get_screenshare_position() -> np.array:
+def get_working_area(img: np.array) -> np.array:
+    """ Removes reduntant image parts (first and fourth y-axis quarter) since
+    the icebergs in the game only appear on the middle two quarters of the image.
+
+    :param img: Source image to be cropped
+    :return: The image with the croppet first and fourth quarter of the y-axis
+    """
+    # keep first and last quarter of the image
+    hh, _ = img.shape[:2]
+    crop_of_relevant_area = img[int(hh/4):int(hh-(hh/4)),]
+
+    # write result to disk
+    # cv2.imwrite("temp/screenshots/crop_of_relevant_area.jpg", crop_of_relevant_area)
+
+    return crop_of_relevant_area
+
+
+def get_screenshare() -> np.array:
+    """Crops the screenshare from the screenshot image.
+
+    :return: An image of the cropped screenshare.
+    """
     img = cv2.imread('temp/screenshots/screenshot.png')
 
     # get color bounds of green box
@@ -12,16 +33,19 @@ def get_screenshare_position() -> np.array:
     # create the black and white mask
     bw_mask = cv2.inRange(img, lower, upper)
 
-    # get bounds of green pixels
+    # get the diagonal endpoints of the white mask
     white_loc_in_mask = np.where(bw_mask == 255)
     xmin, ymin, xmax, ymax = np.min(white_loc_in_mask[1]), np.min(
         white_loc_in_mask[0]), np.max(white_loc_in_mask[1]), np.max(white_loc_in_mask[0])
 
     # crop the image at the bounds
-    crop = img[ymin:ymax, xmin:xmax]
+    crop_of_screenshare = img[ymin:ymax, xmin:xmax]
 
     # write result to disk
     # cv2.imwrite("temp/screenshots/green_box_mask.jpg", mask)
     # cv2.imwrite("temp/screenshots/green_box_cropped.jpg", crop)
 
-    return crop
+    return crop_of_screenshare
+
+i = get_screenshare()
+get_working_area(i)

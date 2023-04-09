a4d037c2cd024ee46a1c274d1abbcec8ee928f1e
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-09 13:42:06 +0200

    feat: Canny edge detection

--- python/image_processing/edge_detection.py
+++ python/image_processing/edge_detection.py
@@ -1,15 +1,29 @@
 import cv2
 import numpy as np
+from crop_to_working_area import crop_image_to_working_area
 
 
-def edge_detection(img: np.array):
-    img = get_screenshare()
-    
-    # convert to grayscale
+def edge_detection() -> np.array:
+    """Performs Canny edge detection on an image.
+
+    :return: A b&w image with the edges in white and background in black.
+    """
+    # get image
+    img = crop_image_to_working_area()
+
+    # convert image to grayscale
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # set lower and upper threshold (found by trial and error)
+    t_lower = 50
+    t_upper = 150
+
     # find edges with Canny
-    edges = cv2.Canny(img, 10, 20, apertureSize=3)
-    # show and save the result
-    cv2.imshow("edges", edges)
-    cv2.waitKey(0)
+    edges = cv2.Canny(img, t_lower, t_upper)
+    
+    # show result
+    # cv2.imshow("edges", edges)
+    # cv2.waitKey(0)
+
+    return edges
+

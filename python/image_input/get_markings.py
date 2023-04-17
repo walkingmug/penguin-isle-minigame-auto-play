e36f24a76b93ee17893ef18cb08553029b300663
walkingmug <walkingmug@users.noreply.github.com~>
2023-04-17 19:40:25 +0200

    feat: display mark on image click

--- python/image_input/get_markings.py
+++ python/image_input/get_markings.py
@@ -20,7 +20,8 @@ def click_event(event, x, y, flags, params) -> None:
     """
     global click_counter
     global x1, y1, x2, y2
-
+    global img 
+    
     # Get click coordinates and stop after two clicks
     if event == cv2.EVENT_LBUTTONDOWN:      
         click_counter += 1
@@ -31,24 +32,26 @@ def click_event(event, x, y, flags, params) -> None:
             x2 = x
             y2 = y
             cv2.destroyAllWindows()
+        
+        # draw a mark on left click
+        cv2.circle(img, center=(x,y), radius=3, color=(0, 0, 255), thickness=-1)
+        cv2.imshow('Mark source and destination', img)
 
 
 def get_markings() -> int:
     """Lets the user input two marks on the image.
     """
+    global img
+    
     # Read and display the image
     img = crop_image_to_working_area()
-    cv2.imshow('image', img)
+    cv2.imshow('Mark source and destination', img)
 
     # Read the markings
-    cv2.setMouseCallback('image', click_event)
-
-    cv2.circle(img, center=(x1,y1), radius=50, color=(0, 255, 0), thickness=-1)
+    cv2.setMouseCallback('Mark source and destination', click_event)
 
     # wait for a key to be pressed to exit
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     return x1, y1, x2, y2
-
-get_markings()

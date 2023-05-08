48136c3061f64de003b0aa8b82c8178011255980
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 18:12:33 +0200

    refactor: add constant for formula

--- python/distance_calculator/convert_distance.py
+++ python/distance_calculator/convert_distance.py
@@ -4,6 +4,7 @@ def get_push_duration_from_distance(pixel_distance) -> float:
     :param pixel_distance: Euclidean pixel distance between the two image marks
     :return: push duration in seconds for the servo to push
     """
-    push_duration = round((pixel_distance/100/1.8), 2)
+    DURATION_FORMULA = 1/(100/1.8)
+    push_duration = round((pixel_distance*DURATION_FORMULA), 2)
 
     return push_duration

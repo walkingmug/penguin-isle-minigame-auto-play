a1e6f6cae5fd0e2f74c6a7c4e0dd16aa3d456e2a
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 20:48:08 +0200

    refactor: move constants to the top

--- python/gui/app.py
+++ python/gui/app.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 300
+
+
 class ImageDisplayGUI:
     def __init__(self, image_path='python/gui/resources/images/no_image_available.png'):
         self.root = tk.Tk()
@@ -12,9 +16,11 @@ class ImageDisplayGUI:
         self.photo = ImageTk.PhotoImage(self.current_image)
 
         self.canvas = tk.Canvas(
-            self.root, width=self.current_image.width, height=self.current_image.height)
+            self.root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
         self.canvas.pack()
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
 
         self.root.mainloop()
+
+
 a = ImageDisplayGUI()

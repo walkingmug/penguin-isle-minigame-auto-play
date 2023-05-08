652c2237ce44aa017a10690a2258dd9e2eb4048c
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 20:37:28 +0200

    feat: initialize window with image

--- python/gui/app.py
+++ python/gui/app.py
@@ -1,6 +1,9 @@
-import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk    
 
 
 class ImageDisplayGUI:
-    def __init__(self):
-        pass
+    def __init__(self, image_path='resources/images/no_image_available.png'):
+        self.root = tk.Tk()
+        self.root.title("Jump Jump Auto Player")
+        self.current_image = image_path

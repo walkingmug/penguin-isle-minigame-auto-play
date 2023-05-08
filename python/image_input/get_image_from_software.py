5dbd7891ee449f63c9d345b642b237394d54d9f9
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 18:34:55 +0200

    refactor: add module docstring and return value

--- python/image_input/get_image_from_software.py
+++ python/image_input/get_image_from_software.py
@@ -4,29 +4,26 @@
 import win32gui
 import win32con
 import time
+import PIL.Image
 from PIL import ImageGrab
 
 
-def get_image_from_software(window_name="Zoom - Google Chrome") -> None:
-    """Takes a screenshot from a software window and saves it.
+def get_image_from_software(window_name="Zoom - Google Chrome") -> PIL.Image:
+    """Takes a screenshot from a software window.
 
-    :raises ValueError: if the window is not found
+    :raises ValueError: If the window is not found
     """
     # Find the handle of the window
     zoom_hwnd = win32gui.FindWindow(None, window_name)
     if zoom_hwnd == 0:
         raise ValueError(f"Window \"{window_name}\" does not exist.")
 
-    # Display the window so that it can take a screenshot
+    # Display the window
     win32gui.ShowWindow(zoom_hwnd, win32con.SW_SHOWNORMAL)
-
-    # Give some time for the window to show
     time.sleep(1)
 
-    # Get the coordinates of the window
+    # Get the coordinates of the window and take a screenshot
     (left, top, right, bottom) = win32gui.GetWindowRect(zoom_hwnd)
-
-    # Take a screenshot of the window
     screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
 
     # Minimize the window
@@ -34,3 +31,5 @@ def get_image_from_software(window_name="Zoom - Google Chrome") -> None:
 
     # Save the screenshot to a file
     screenshot.save('temp\screenshots\screenshot.png')
+
+    return screenshot

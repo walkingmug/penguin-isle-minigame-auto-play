aeed6f1491342884ebc0a5eaa07356117121638b
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 20:52:40 +0200

    refactor: move no image found path to constant

--- python/gui/app.py
+++ python/gui/app.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+import numpy as np
 from PIL import Image, ImageTk
 
 
 CANVAS_WIDTH = 400
 CANVAS_HEIGHT = 300
+NO_IMAGE_FOUND_PATH = 'python/gui/resources/images/no_image_available.png'
 
 
 class ImageDisplayGUI:
-    def __init__(self, image_path='python/gui/resources/images/no_image_available.png'):
+    def __init__(self, image_path=NO_IMAGE_FOUND_PATH):
         self.root = tk.Tk()
         self.root.title("Jump Jump Auto Player")
         self.image_path = image_path
@@ -21,6 +23,9 @@ class ImageDisplayGUI:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
 
         self.root.mainloop()
+
+    def set_image(self, image: np.array):
+
 
 
 a = ImageDisplayGUI()

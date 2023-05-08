d139023306c5d6344af6f352924f3d86bfe8fa6b
walkingmug <walkingmug@users.noreply.github.com~>
2023-05-08 17:59:54 +0200

    refactor: renaming of vars for better description

--- python/distance_calculator/calculate_distance.py
+++ python/distance_calculator/calculate_distance.py
@@ -1,12 +1,12 @@
-def get_distance_in_pixels(x1, y1, x2, y2) -> float:
-    """Calculates the pixel distance between two marks in an image.
+def get_distance_in_pixels(x_src, y_src, x_dest, y_dest) -> float:
+    """Calculates the pixel distance (Euclidean) between two marks in an image.
 
-    :param x1: X-value of mark 1.
-    :param y1: Y-value of mark 1.
-    :param x2: X-value of mark 2.
-    :param y2: Y-value of mark 2.
+    :param x_src: X-value of source mark.
+    :param y_src: Y-value of source mark.
+    :param x_dest: X-value of destination mark.
+    :param y_dest: Y-value of destination mark.
     :return: The pixel distance between two marks.
     """
-    # get coordinates of marks
-    distance = ((int(x2) - int(x1))**2 + (int(y2) - int(y1))**2)**0.5
-    return distance
+    pixel_distance = ((int(x_dest) - int(x_src))**2 +
+                      (int(y_dest) - int(y_src))**2)**0.5
+    return pixel_distance

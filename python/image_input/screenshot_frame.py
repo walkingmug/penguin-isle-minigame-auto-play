ecc6bd2d39a030e3a720a52aba4b460cdd78f1c1
walkingmug <walkingmug@users.noreply.github.com>
2023-07-09 16:55:48 +0200

    fix: pass manual arguments

--- python/image_input/screenshot_frame.py
+++ python/image_input/screenshot_frame.py
@@ -1,9 +1,15 @@
 """Classes for capturing screenshots and performing image processing. 
 """
 
-from python.image_processing.image_transformation.crop_to_working_area import crop_image_to_working_area
-from python.image_processing.object_detection.source_detection import get_center_of_source_iceberg
-from python.image_processing.object_detection.destination_detection import get_center_of_destination_iceberg
+from python.image_processing.image_transformation.crop_to_working_area import (
+    crop_image_to_working_area,
+)
+from python.image_processing.object_detection.source_detection import (
+    get_center_of_source_iceberg,
+)
+from python.image_processing.object_detection.destination_detection import (
+    get_center_of_destination_iceberg,
+)
 from python.image_input.draw_markings import draw_mark
 from python.image_input.get_markings import get_markings
 import cv2
@@ -23,21 +29,25 @@ class ScreenshotFrame:
         # self.current_frame = cv2.imread(
         # "temp/screenshots/crop_of_relevant_area.jpg")
 
-    def find_source(self):
+    def find_source(self, manual=False):
         self.x_src, self.y_src = get_center_of_source_iceberg(
-            self.current_frame)
+            self.current_frame, manual=manual
+        )
 
-    def find_destination(self):
+    def find_destination(self, manual=False):
         self.x_dest, self.y_dest = get_center_of_destination_iceberg(
-            self.current_frame)
+            self.current_frame, manual=manual
+        )
 
     def draw_source_on_frame(self):
         self.current_frame = draw_mark(
-            self.current_frame, self.x_src, self.y_src, 'red')
+            self.current_frame, self.x_src, self.y_src, "red"
+        )
 
     def draw_destination_on_frame(self):
         self.current_frame = draw_mark(
-            self.current_frame, self.x_dest, self.y_dest, 'green')
+            self.current_frame, self.x_dest, self.y_dest, "green"
+        )
 
     def update_frame_with_src_and_dest(self):
         self.draw_destination_on_frame()

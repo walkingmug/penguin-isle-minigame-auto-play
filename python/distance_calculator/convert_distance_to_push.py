ea6c97d13bee85cbccee67e1d38bcc2809a58ff6
walkingmug <walkingmug@users.noreply.github.com>
2023-07-11 19:55:29 +0200

    style: fix comment consistency

--- python/distance_calculator/convert_distance_to_push.py
+++ python/distance_calculator/convert_distance_to_push.py
@@ -5,10 +5,10 @@
 def get_push_duration_from_distance(percent_distance) -> float:
     """Converts the pixel distance (Euclidean) between two points into seconds.
 
-    :param percent_distance: Euclidean percent distance between the two image marks
-    :return: push duration in seconds for the servo to push
+    :param percent_distance: Euclidean percent distance between the two marks
+    :return: Push duration in seconds for the servo to push
     """
-    DURATION_FORMULA = 0.95 #1/(100/0.5)
-    push_duration = round((percent_distance*DURATION_FORMULA), 2)
+    DURATION_FORMULA = 0.95
+    push_duration = round((percent_distance * DURATION_FORMULA), 2)
 
     return push_duration

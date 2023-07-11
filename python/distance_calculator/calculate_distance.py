892d5858947a331c2da9e7fa98cf6aa5e6acbba2
walkingmug <walkingmug@users.noreply.github.com>
2023-07-11 19:50:34 +0200

    feat: remove straight distance calculator

--- python/distance_calculator/calculate_distance.py
+++ python/distance_calculator/calculate_distance.py
@@ -1,10 +1,6 @@
 """Functions for calculating the distance between two pixels.
 """
 
-def get_straight_distance(x_src, y_src, x_dest, y_dest):
-    print(x_dest, x_src)
-    return abs(x_dest-x_src)
-
 
 def get_euclidean_distance(x_src, y_src, x_dest, y_dest) -> float:
     """Calculates the pixel distance (Euclidean) between two marks in an image.
@@ -15,6 +11,5 @@ def get_euclidean_distance(x_src, y_src, x_dest, y_dest) -> float:
     :param y_dest: Y-value of destination mark
     :return: The pixel distance between two marks
     """
-    pixel_distance = ((x_dest - x_src)**2 +
-                      (y_dest - y_src)**2)**0.5
+    pixel_distance = ((x_dest - x_src) ** 2 + (y_dest - y_src) ** 2) ** 0.5
     return pixel_distance

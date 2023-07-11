db3b8e26f26bfd2b5097a0e588c8f29491a78e9c
walkingmug <walkingmug@users.noreply.github.com>
2023-07-11 23:15:30 +0200

    style: black formatting

--- python/image_input/get_markings.py
+++ python/image_input/get_markings.py
@@ -2,12 +2,11 @@
 """
 
 import cv2
-from python.image_processing.image_transformation.crop_to_working_area import crop_image_to_working_area
 from python.image_input.draw_markings import draw_mark
 
 
-click_type = ''
-img_title = ''
+click_type = ""
+img_title = ""
 x1 = None
 y1 = None
 x2 = None
@@ -30,25 +29,26 @@ def click_event(event, x, y, flags, params) -> None:
 
     # Get click coordinates and stop after two clicks
     if event == cv2.EVENT_LBUTTONDOWN:
-        if click_type == 'src':
+        if click_type == "src":
             x1 = x
             y1 = y
-            draw_mark(img, x1, y1, 'red')
-        elif click_type == 'dest':
+            draw_mark(img, x1, y1, "red")
+        elif click_type == "dest":
             x2 = x
             y2 = y
-            draw_mark(img, x2, y2, 'green')
+            draw_mark(img, x2, y2, "green")
         else:
-            raise ValueError(f"Variable 'click_type' must be either 'src' \
-                             or 'dest', but '{click_type}' was given.")
-        click_type = ''
+            raise ValueError(
+                f"Variable 'click_type' must be either 'src' \
+                             or 'dest', but '{click_type}' was given."
+            )
+        click_type = ""
         cv2.destroyAllWindows()
         cv2.imshow(img_title, img)
 
 
 def get_markings(cropped_img, mark_src=False, mark_dest=False) -> int:
-    """Lets the user input two marks on the image.
-    """
+    """Lets the user input two marks on the image."""
     global img
     global click_type
     global img_title
@@ -56,18 +56,18 @@ def get_markings(cropped_img, mark_src=False, mark_dest=False) -> int:
     img = cropped_img
 
     # get source/destination mark from user
-    if mark_src == True:
-        click_type = 'src'
-        img_title = 'Mark source'
+    if mark_src is True:
+        click_type = "src"
+        img_title = "Mark source"
         cv2.imshow(img_title, img)
         cv2.setMouseCallback(img_title, click_event)
-    if  mark_dest == True:
-        click_type = 'dest'
-        img_title = 'Mark destination'
+    if mark_dest is True:
+        click_type = "dest"
+        img_title = "Mark destination"
         cv2.imshow(img_title, img)
         cv2.setMouseCallback(img_title, click_event)
     # auto get source and destination marks
-    if mark_src == False and mark_dest == False:
+    if mark_src is False and mark_dest is False:
         return -1
 
     # wait for a key to be pressed to exit

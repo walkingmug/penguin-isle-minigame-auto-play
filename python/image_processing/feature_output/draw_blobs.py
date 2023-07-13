d4c7b9e5e0797900a792d117c02371037078583e
walkingmug <walkingmug@users.noreply.github.com>
2023-07-13 20:30:37 +0200

    feat: return image

--- python/image_processing/feature_output/draw_blobs.py
+++ python/image_processing/feature_output/draw_blobs.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def draw_keypoint_circles(src_img: np.array, src_kpts, dest_kpts) -> None:
     # draw the source blob as red
-    img_with_keypoints = cv2.drawKeypoints(
+    img_has_src_kpts = cv2.drawKeypoints(
         src_img,
         (src_kpts,),
         np.array([]),
@@ -13,10 +13,12 @@ def draw_keypoint_circles(src_img: np.array, src_kpts, dest_kpts) -> None:
     )
 
     # draw the destination blob as green
-    img_with_keypoints = cv2.drawKeypoints(
-        img_with_keypoints,
+    img_has_dest_kpts = cv2.drawKeypoints(
+        img_has_src_kpts,
         dest_kpts,
         np.array([]),
         (0, 255, 0),
         cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
     )
+
+    return img_has_dest_kpts

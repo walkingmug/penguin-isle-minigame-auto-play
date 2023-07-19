d3cdf5dc9ecaa9c00e3eff38e0d725d63a46b869
walkingmug <walkingmug@users.noreply.github.com>
2023-07-19 21:34:38 +0200

    fix: convert UMat to numpy

--- tests/test_image_input/test_draw_markings.py
+++ tests/test_image_input/test_draw_markings.py
@@ -1,11 +1,11 @@
 from python.image_input import draw_markings
-import numpy
+import numpy as np
 import pytest
 
 
 @pytest.mark.parametrize("color", ["red", "green", "blue"])
 def test_can_change_value_on_numpy_array(color):
-    img = numpy.random.rand(50, 50, 3)
+    img = np.random.rand(50, 50, 3)
     x, y = 30, 40
     color_map = {"red": [0, 0, 255], "green": [0, 255, 0], "blue": [255, 0, 0]}
 
@@ -13,4 +13,6 @@ def test_can_change_value_on_numpy_array(color):
     img_expected[x, y] = color_map[color]
     img_result = draw_markings.draw_mark(img, x, y, color)
 
-    assert img_result.all() == img_expected.all()
+    assert (
+        img_result.get().all() == img_expected.all()
+    ), "Failed to draw mark properly."
